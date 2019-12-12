665cdb47f09d0f8fca2eda330fa6fd1ad2f8b48c
Achim Fritz <achim.fritz@b13.de>
2019-12-12 07:44:00 +0100

    [TASK] init

---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Container',
+    'description' => 'Container Content Elements',
+    'category' => 'misc',
+    'author' => 'b13 GmbH',
+    'author_email' => 'typo3@b13.de',
+    'author_company' => 'b13 GmbH',
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'createDirs' => '',
+    'clearCacheOnLoad' => true,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => ['typo3' => '10.0-10.9'],
+        'conflicts' => [],
+        'suggests' => []
+    ]
+];
+
