0dbc73cad77c160bfd4f3ee2daeddc791e2940e3
Achim Fritz <achim.fritz@b13.de>
2019-12-17 07:38:29 +0100

    [TASK] rm example data

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -29,24 +29,3 @@ $GLOBALS['TCA']['tt_content']['columns']['colPos']['config']['itemsProcFunc'] = 
 // copyAfterDuplFields colPos,sys_language_uid
 // useColumnsForDefaultValues colPos,sys_language_uid,CType
 $GLOBALS['TCA']['tt_content']['ctrl']['useColumnsForDefaultValues'] .= ',tx_container_parent';
-
-
-
-\B13\Container\TcaRegistry::registerContainer(
-    'foo',
-    'bar',
-    'bar-description',
-    'container',
-    [
-        [
-            ['name' => 'foo', 'colPos' => 100, 'colspan' => 2]
-        ],
-        [
-            ['name' => 'foo2', 'colPos' => 101],
-            ['name' => 'foo3', 'colPos' => 102]
-        ]
-    ]
-);
-
-$GLOBALS['TCA']['tt_content']['types']['foo']['showitem'] = 'sys_language_uid,CType,header,tx_container_parent,colPos';
-
