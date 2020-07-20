41b9c090809cce1b0ae06b2420b5a2ae40e5db53
Benni Mack <benjamin.mack@b13.com>
2020-07-20 12:35:12 +0200

    [TASK] Prepare 1.1.1 release

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => false,
     'createDirs' => '',
     'clearCacheOnLoad' => true,
-    'version' => '1.1.0',
+    'version' => '1.1.1',
     'constraints' => [
         'depends' => ['typo3' => '9.5.0-10.4.99'],
         'conflicts' => [],
