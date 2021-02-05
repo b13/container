90002eb4583d168cd8b1401474086c1c25ab5956
Achim Fritz <af@achimfritz.de>
2021-02-05 10:31:16 +0100

    [TASK] testing v10 and v11

--- Tests/Acceptance/Support/Extension/BackendContainerEnvironment.php
+++ Tests/Acceptance/Support/Extension/BackendContainerEnvironment.php
@@ -49,4 +49,12 @@ class BackendContainerEnvironment extends BackendEnvironment
             'EXT:container/Tests/Acceptance/Fixtures/be_groups.xml'
         ],
     ];
+
+    public function _initialize()
+    {
+        if (getenv('FLUID_BASED_PAGE_MODULE')) {
+            $this->localConfig['configurationToUseInTestInstance']['SYS']['features']['fluidBasedPageModule'] = true;
+        }
+        parent::_initialize();
+    }
 }
