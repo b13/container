757ac6344789c7f70fc3335b928a0e84139145a0
Oliver Bartsch <bo@cedev.de>
2021-07-21 12:12:58 +0200

    [RELEASE] Version 1.3.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => false,
     'createDirs' => '',
     'clearCacheOnLoad' => true,
-    'version' => '1.2.0',
+    'version' => '1.3.1',
     'constraints' => [
         'depends' => ['typo3' => '9.5.0-11.99.99'],
         'conflicts' => [],
