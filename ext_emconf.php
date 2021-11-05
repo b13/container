5ea1b22f3469ead43994f2a7b53c60fd14f3f1cc
Achim Fritz <achim.fritz@b13.de>
2021-11-05 16:49:12 +0100

    [RELEASE] 1.4.0 Add support for PHP8

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => false,
     'createDirs' => '',
     'clearCacheOnLoad' => true,
-    'version' => '1.3.1',
+    'version' => '1.4.0',
     'constraints' => [
         'depends' => ['typo3' => '9.5.0-11.99.99'],
         'conflicts' => [],
