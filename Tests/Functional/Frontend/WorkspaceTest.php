e24d8dc8ec78f81974453915258110764db1f7c8
Achim Fritz <achim.fritz@b13.de>
2021-11-15 09:39:16 +0100

    Revert "[WIP] workspace tests"
    This reverts commit 1dad46274334a01fa58200d5e3f617c7a6bdcda2.

--- Tests/Functional/Frontend/WorkspaceTest.php
+++ Tests/Functional/Frontend/WorkspaceTest.php
@@ -104,8 +104,7 @@ class WorkspaceTest extends AbstractFrontendTest
     public function childInWorkspaceIsRenderendIfContainerIsMovedToOtherPage(): void
     {
         if ($this->typo3MajorVersion === 11) {
-            // todo WorkspacePreview Notice error is triggered
-            $GLOBALS['LANG'] = null;
+            self::markTestSkipped('todo WorkspacePreview Notice error is triggered');
         }
         $this->importDataSet(ORIGINAL_ROOT . 'typo3conf/ext/container/Tests/Functional/Fixtures/Workspace/other_page.xml');
         if ($this->typo3MajorVersion < 11) {
@@ -126,6 +125,9 @@ class WorkspaceTest extends AbstractFrontendTest
      */
     public function containerInWorkspaceIsRenderedWhenLiveVersionIsHidden(): void
     {
+        if ($this->typo3MajorVersion === 11) {
+            self::markTestSkipped('todo WorkspacePreview Notice error is triggered');
+        }
         $this->importDataSet(ORIGINAL_ROOT . 'typo3conf/ext/container/Tests/Functional/Fixtures/Workspace/container_in_ws_whith_hidden_live_version.xml');
         $context = (new InternalRequestContext())->withWorkspaceId(1)->withBackendUserId(1);
         $response = $this->executeFrontendRequest(new InternalRequest(), $context);
