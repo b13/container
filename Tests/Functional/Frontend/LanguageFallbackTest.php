9b65feea39ca53a227b5a218897b9af75f7f1b79
Achim Fritz <achim.fritz@b13.de>
2023-01-13 13:03:05 +0100

    [BUGFIX] language fallback
    use container language for children in free mode
    
    Fixes: #340

--- Tests/Functional/Frontend/LanguageFallbackTest.php
+++ Tests/Functional/Frontend/LanguageFallbackTest.php
@@ -68,6 +68,25 @@ class LanguageFallbackTest extends AbstractFrontendTest
      * @test
      * @group frontend
      */
+    public function fallbackForStrictLanguageToOtherTranslationFreeMode(): void
+    {
+        $this->importCSVDataSet(ORIGINAL_ROOT . 'typo3conf/ext/container/Tests/Functional/Frontend/Fixtures/LanguageFallback/tt_content_fallback_for_strict_language_to_other_translation_free_mode.csv');
+        $response = $this->executeFrontendRequestWrapper(new InternalRequest('http://localhost/ch'));
+        $body = (string)$response->getBody();
+        $body = $this->prepareContent($body);
+        self::assertStringContainsString('<h1 class="container">container-de</h1>', $body);
+        self::assertStringNotContainsString('<h1 class="container">container-default</h1>', $body);
+        self::assertStringContainsString('<h6 class="header-children">header-de</h6>', $body);
+        self::assertStringNotContainsString('<h6 class="header-children">header-default</h6>', $body);
+        // rendered content
+        self::assertStringContainsString('<h2 class="">header-de</h2>', $body);
+        self::assertStringNotContainsString('<h2 class="">header-default</h2>', $body);
+    }
+
+    /**
+     * @test
+     * @group frontend
+     */
     public function bothTranslatedTranslatedChildHidden(): void
     {
         $this->importCSVDataSet(ORIGINAL_ROOT . 'typo3conf/ext/container/Tests/Functional/Frontend/Fixtures/LanguageFallback/tt_content_both_translated_tranlated_child_hidden.csv');
