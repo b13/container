576a761734c2d173463a67393337303cfc0d9fe6
Achim Fritz <achim.fritz@b13.de>
2023-10-17 08:44:28 +0200

    [RELEASE] 2.3.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => false,
     'createDirs' => '',
     'clearCacheOnLoad' => true,
-    'version' => '2.3.0',
+    'version' => '2.3.1',
     'constraints' => [
         'depends' => ['typo3' => '10.4.18-12.99.99'],
         'conflicts' => [],
