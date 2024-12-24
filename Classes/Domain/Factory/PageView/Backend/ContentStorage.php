fa1bb983c4c3c4a1f6e68dc803809160d60e0829
Achim Fritz <af@achimfritz.de>
2024-12-24 10:48:01 +0100

    [TASK] use ContentObjectRenderer
    to get container child records for FE
    
    cleanup pageview layer
    unused method in database

--- Classes/Domain/Factory/PageView/Backend/ContentStorage.php
+++ Classes/Domain/Factory/PageView/Backend/ContentStorage.php
@@ -12,11 +12,74 @@ namespace B13\Container\Domain\Factory\PageView\Backend;
  * of the License, or any later version.
  */
 
+use B13\Container\Domain\Factory\Database;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Context\Context;
 use TYPO3\CMS\Core\Versioning\VersionState;
 
-class ContentStorage extends \B13\Container\Domain\Factory\PageView\ContentStorage
+class ContentStorage
 {
+    /**
+     * @var ?mixed[]
+     */
+    protected $records;
+
+    /**
+     * @var Database
+     */
+    protected $database;
+
+    /**
+     * @var int
+     */
+    protected $workspaceId = 0;
+
+    public function __construct(Database $database, Context $context)
+    {
+        $this->database = $database;
+        $this->workspaceId = (int)$context->getPropertyFromAspect('workspace', 'id');
+    }
+
+    protected function buildRecords(int $pid, int $language): array
+    {
+        $records = $this->database->fetchRecordsByPidAndLanguage($pid, $language);
+        $records = $this->workspaceOverlay($records);
+        $records = $this->recordsByContainer($records);
+        return $records;
+    }
+
+    protected function recordsByContainer(array $records): array
+    {
+        $recordsByContainer = [];
+        foreach ($records as $record) {
+            if ($record['tx_container_parent'] > 0) {
+                if (empty($recordsByContainer[$record['tx_container_parent']])) {
+                    $recordsByContainer[$record['tx_container_parent']] = [];
+                }
+                $recordsByContainer[$record['tx_container_parent']][] = $record;
+            }
+        }
+        return  $recordsByContainer;
+    }
+
+    public function getContainerChildren(array $containerRecord, int $language): array
+    {
+        $pid = (int)$containerRecord['pid'];
+        if (isset($containerRecord['t3ver_oid']) && $containerRecord['t3ver_oid'] > 0) {
+            $defaultContainerRecord = $this->database->fetchOneRecord((int)$containerRecord['t3ver_oid']);
+            $uid = (int)$defaultContainerRecord['uid'];
+        } else {
+            $uid = (int)$containerRecord['uid'];
+        }
+        if (!isset($this->records[$pid][$language])) {
+            $this->records[$pid][$language] = $this->buildRecords($pid, $language);
+        }
+        if (empty($this->records[$pid][$language][$uid])) {
+            return [];
+        }
+        return $this->records[$pid][$language][$uid];
+    }
+
     public function workspaceOverlay(array $records): array
     {
         $filtered = [];
--- Tests/Unit/Domain/Factory/PageView/Backend/ContainerFactoryTest.php
+++ Tests/Unit/Domain/Factory/PageView/Backend/ContainerFactoryTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace B13\Container\Tests\Unit\Domain\Factory\PageView;
+namespace B13\Container\Tests\Unit\Domain\Factory\PageView\Backend;
 
 /*
  * This file is part of TYPO3 CMS-based extension "container" by b13.
@@ -13,7 +13,8 @@ namespace B13\Container\Tests\Unit\Domain\Factory\PageView;
  */
 
 use B13\Container\Domain\Factory\Database;
-use B13\Container\Domain\Factory\PageView\ContainerFactory;
+use B13\Container\Domain\Factory\PageView\Backend\ContainerFactory;
+use B13\Container\Domain\Factory\PageView\Backend\ContentStorage;
 use B13\Container\Tca\Registry;
 use TYPO3\CMS\Core\Context\Context;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
@@ -34,8 +35,9 @@ class ContainerFactoryTest extends UnitTestCase
         $database->expects(self::once())->method('fetchOneRecord')->with(1)->willReturn(null);
         $tcaRegistry = $this->getMockBuilder(Registry::class)->disableOriginalConstructor()->getMock();
         $context = $this->getMockBuilder(Context::class)->getMock();
+        $contentStorage = $this->getMockBuilder(ContentStorage::class)->disableOriginalConstructor()->getMock();
         $containerFactory = $this->getMockBuilder($this->buildAccessibleProxy(ContainerFactory::class))
-            ->setConstructorArgs(['database' => $database, 'tcaRegistry' => $tcaRegistry, 'context' => $context])
+            ->setConstructorArgs(['database' => $database, 'tcaRegistry' => $tcaRegistry, 'context' => $context, 'contentStorage' => $contentStorage])
             ->onlyMethods([])
             ->getMock();
         self::assertNull($containerFactory->_call('containerByUid', 1));
