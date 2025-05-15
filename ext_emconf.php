704805c3c6ec70135f8b5d05333a332a1d50bdd4
Achim Fritz <af@achimfritz.de>
2025-05-15 10:03:24 +0200

    [RELEASE] 3.1.9

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => false,
     'createDirs' => '',
     'clearCacheOnLoad' => true,
-    'version' => '3.1.8',
+    'version' => '3.1.9',
     'constraints' => [
         'depends' => ['typo3' => '11.5.0-13.99.99'],
         'conflicts' => [],
