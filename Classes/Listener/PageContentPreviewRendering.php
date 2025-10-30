7c19af69607593e7072e26453617d829fe0ef8f6
Achim Fritz <af@achimfritz.de>
2025-10-30 18:49:25 +0100

    [TASK] PageContentPreviewRenderingEvent changed in v14
    property record changed from array to RecordInterface
    
    s. https://review.typo3.org/c/Packages/TYPO3.CMS/+/88519

--- Classes/Listener/PageContentPreviewRendering.php
+++ Classes/Listener/PageContentPreviewRendering.php
@@ -15,6 +15,8 @@ namespace B13\Container\Listener;
 use B13\Container\Backend\Preview\GridRenderer;
 use B13\Container\Tca\Registry;
 use TYPO3\CMS\Backend\View\Event\PageContentPreviewRenderingEvent;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class PageContentPreviewRendering
 {
@@ -34,10 +36,21 @@ class PageContentPreviewRendering
         }
 
         $record = $event->getRecord();
-        if (!$this->tcaRegistry->isContainerElement((string)$record['CType'])) {
+
+        if ((GeneralUtility::makeInstance(Typo3Version::class))->getMajorVersion() > 13) {
+            $recordType = $record->getRecordType();
+        } else {
+            $recordType = $record['CType'];
+        }
+        if (!$this->tcaRegistry->isContainerElement($recordType)) {
             return;
         }
-        $record['tx_container_grid'] = $this->gridRenderer->renderGrid($record, $event->getPageLayoutContext());
-        $event->setRecord($record);
+        if ((GeneralUtility::makeInstance(Typo3Version::class))->getMajorVersion() > 13) {
+            $previewContent = $this->gridRenderer->renderGrid($record->toArray(), $event->getPageLayoutContext());
+            $event->setPreviewContent($previewContent);
+        } else {
+            $record['tx_container_grid'] = $this->gridRenderer->renderGrid($record, $event->getPageLayoutContext());
+            $event->setRecord($record);
+        }
     }
 }
