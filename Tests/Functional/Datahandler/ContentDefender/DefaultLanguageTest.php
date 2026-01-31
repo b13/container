61c1c4c65d61ea92a6906da55f578b43cdf3d9f9
Achim Fritz <af@achimfritz.de>
2026-01-31 15:29:48 +0100

    [FEATURE] ce restriction datahandler hook

--- Tests/Functional/Datahandler/ContentDefender/DefaultLanguageTest.php
+++ Tests/Functional/Datahandler/ContentDefender/DefaultLanguageTest.php
@@ -12,6 +12,8 @@ namespace B13\Container\Tests\Functional\Datahandler\ContentDefender;
  * of the License, or any later version.
  */
 
+use TYPO3\CMS\Core\Information\Typo3Version;
+
 class DefaultLanguageTest extends AbstractContentDefender
 {
     /**
@@ -20,11 +22,13 @@ class DefaultLanguageTest extends AbstractContentDefender
     protected array $testExtensionsToLoad = [
         'typo3conf/ext/container',
         'typo3conf/ext/container_example',
-        'typo3conf/ext/content_defender',
     ];
 
     protected function setUp(): void
     {
+        if ((new Typo3Version())->getMajorVersion() < 14) {
+            $this->testExtensionsToLoad[] = 'typo3conf/ext/content_defender';
+        }
         parent::setUp();
         $this->importCSVDataSet(__DIR__ . '/Fixtures/DefaultLanguage/setup.csv');
     }
--- Tests/Functional/Datahandler/ContentDefender/MaxItemsTest.php
+++ Tests/Functional/Datahandler/ContentDefender/MaxItemsTest.php
@@ -12,6 +12,7 @@ namespace B13\Container\Tests\Functional\Datahandler\ContentDefender;
  * of the License, or any later version.
  */
 
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\StringUtility;
 
 class MaxItemsTest extends AbstractContentDefender
@@ -22,11 +23,13 @@ class MaxItemsTest extends AbstractContentDefender
     protected array $testExtensionsToLoad = [
         'typo3conf/ext/container',
         'typo3conf/ext/container_example',
-        'typo3conf/ext/content_defender',
     ];
 
     protected function setUp(): void
     {
+        if ((new Typo3Version())->getMajorVersion() < 14) {
+            $this->testExtensionsToLoad[] = 'typo3conf/ext/content_defender';
+        }
         parent::setUp();
         $this->linkSiteConfigurationIntoTestInstance();
     }
