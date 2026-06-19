59652737b453226c6f194363b54690d718c5d966
Achim Fritz <af@achimfritz.de>
2026-06-19 17:37:55 +0200

    [RELEASE] 4.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => false,
     'createDirs' => '',
     'clearCacheOnLoad' => true,
-    'version' => '4.0.0',
+    'version' => '4.0.1',
     'constraints' => [
         'depends' => ['typo3' => '13.4.26-14.99.99'],
         'conflicts' => [],
